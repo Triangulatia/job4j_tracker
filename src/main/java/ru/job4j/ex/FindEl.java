dcdc2e9bab857a5455a7306e0285a2b129349337
master <triangulationtri@gmail.com>
2021-12-20 22:39:21 +0400

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,26 @@
+package ru.job4j.ex;
+
+public class FindEl {
+
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i] != null && key.equals(value[i])) {
+                rsl = i;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Element not found");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+            int rsl = indexOf(new String[] {"not", "fond"}, "found");
+            System.out.println(rsl);
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
